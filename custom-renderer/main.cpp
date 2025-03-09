ad90fb865e31d87dd2f69c8cb1bc72ea0a06ea9f
Kacper Gajko <kacper.gajko1@icloud.com>
2025-03-09 23:23:41 +0100

    Working hello world with metal

--- custom-renderer/main.cpp
+++ custom-renderer/main.cpp
@@ -5,10 +5,21 @@
 //  Created by Kacper on 09/03/2025.
 //
 
+#include <cassert>
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
+
+
+#pragma region Declarations {
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
