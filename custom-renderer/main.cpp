3afb7264282f036fb71a457a3bc4c8ea9c8f5c4c
Kacper Gajko <kacper.gajko1@icloud.com>
2025-03-09 22:54:50 +0100

    Initial Commit

---
+++ custom-renderer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  custom-renderer
+//
+//  Created by Kacper on 09/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
